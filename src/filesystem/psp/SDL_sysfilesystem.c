43c7064661fd01a3c434b67105c65205fff8b4b6
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-11-16 17:03:23 +0100

    Apply original PSP patch

---
+++ src/filesystem/psp/SDL_sysfilesystem.c
@@ -0,0 +1,68 @@
+/*
+  Simple DirectMedia Layer
+  Copyright (C) 1997-2020 Sam Lantinga <slouken@libsdl.org>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+#include "../../SDL_internal.h"
+
+#include <sys/stat.h>
+
+#if defined(SDL_FILESYSTEM_PSP)
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* System dependent filesystem routines                                */
+
+#include "SDL_error.h"
+#include "SDL_filesystem.h"
+
+char *
+SDL_GetBasePath(void)
+{
+  int len = 10;
+  char *retval = malloc(len);
+  SDL_snprintf(retval, len, "ms0:/PSP/");
+  return retval;
+}
+
+char *
+SDL_GetPrefPath(const char *org, const char *app)
+{
+  char *retval = NULL;
+  if (!app) {
+    SDL_InvalidParamError("app");
+    return NULL;
+  }
+
+  const char *base = SDL_GetBasePath();
+  const size_t len = SDL_strlen(SDL_GetBasePath()) + SDL_strlen(org) + SDL_strlen(app) + 4;
+  retval = (char *) SDL_malloc(len);
+    
+  if (*org) {
+    SDL_snprintf(retval, len, "%s/%s/%s/", base, org, app);
+  } else {
+    SDL_snprintf(retval, len, "%s/%s/", base, app);
+  }
+  free(base);
+
+  mkdir(retval, 0700);
+  return retval;
+}
+
+#endif /* SDL_FILESYSTEM_DUMMY || SDL_FILESYSTEM_DISABLED */
+
+/* vi: set ts=4 sw=4 expandtab: */
