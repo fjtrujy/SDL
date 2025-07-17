6205c7157a14e63d8b96b39e1d5f98b98242ebbc
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2025-07-17 23:31:19 +0200

    Encapsulate SDL_RENDERCMD_DRAW_LINES calls

--- src/render/psp/SDL_render_psp.c
+++ src/render/psp/SDL_render_psp.c
@@ -908,22 +908,6 @@ static inline int PSP_RenderGeometry(SDL_Renderer *renderer, void *vertices, SDL
     return 0;
 }
 
-static inline int PSP_RenderLines(SDL_Renderer *renderer, void *vertices, SDL_RenderCommand *cmd)
-{
-    PSP_RenderData *data = (PSP_RenderData *)renderer->driverdata;
-    const size_t count = cmd->data.draw.count;
-    const VertV *verts = (VertV *)(vertices + cmd->data.draw.first);
-    const PSP_BlendInfo blendInfo = {
-        .mode = cmd->data.draw.blend,
-        .shade = GU_FLAT
-    };
-
-    setBlendMode(data, blendInfo);
-    sceGuDrawArray(count > 2 ? GU_LINES : GU_LINE_STRIP, GU_VERTEX_32BITF | GU_TRANSFORM_2D, count, 0, verts);
-
-    return 0;
-}
-
 static inline int PSP_RenderFillRects(SDL_Renderer *renderer, void *vertices, SDL_RenderCommand *cmd)
 {
     PSP_RenderData *data = (PSP_RenderData *)renderer->driverdata;
@@ -1036,7 +1020,38 @@ static int PSP_RunCommandQueue(SDL_Renderer *renderer, SDL_RenderCommand *cmd, v
         }
         case SDL_RENDERCMD_DRAW_LINES:
         {
-            PSP_RenderLines(renderer, vertices, cmd);
+            SDL_RenderCommand *finalcmd = cmd;
+            SDL_RenderCommand *nextcmd = cmd->next;
+            size_t count = cmd->data.draw.count;
+            const VertV *verts = (VertV *)(vertices + cmd->data.draw.first);
+            const PSP_BlendInfo blendInfo = {
+                .mode = cmd->data.draw.blend,
+                .shade = GU_FLAT
+            };
+            void *expectedNextVerts = (void *)((uintptr_t)verts + count * sizeof(VertV));
+            uint8_t gu_primitive = count > 2 ? GU_LINE_STRIP : GU_LINES;
+
+            while (nextcmd) {
+                const SDL_RenderCommandType nextcmdtype = nextcmd->command;
+                const size_t nextcount = nextcmd->data.draw.count;
+                const uint8_t nextgu_primitive = nextcount > 2 ? GU_LINE_STRIP : GU_LINES;
+                if (nextcmdtype != SDL_RENDERCMD_DRAW_LINES || 
+                    gu_primitive != GU_LINES ||
+                    gu_primitive != nextgu_primitive ||
+                    (vertices + nextcmd->data.draw.first) != expectedNextVerts || 
+                    (count + nextcount) > MAX_VERTICES) {
+                    break; /* can't go any further on this draw call */
+                }
+                expectedNextVerts = (void *)((uintptr_t)expectedNextVerts + nextcount * sizeof(VertV));
+                count += nextcount;
+                finalcmd = nextcmd;
+                nextcmd = nextcmd->next;
+            }
+
+            setBlendMode(data, blendInfo);
+            sceGuDrawArray(gu_primitive, GU_VERTEX_32BITF | GU_TRANSFORM_2D, count, 0, verts);
+            cmd = finalcmd;
+
             break;
         }
         case SDL_RENDERCMD_FILL_RECTS:
